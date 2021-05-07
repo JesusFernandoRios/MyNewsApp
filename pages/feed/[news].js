1f0e75845a42bcb994cc1d7d31c905048d75360a
jesus <Jesusfernandorios93@gmail.com>
2021-05-07 15:05:55 -0700

    created next and previous page feature on news feed

--- pages/feed/[news].js
+++ pages/feed/[news].js
@@ -1,18 +1,56 @@
+import { useRouter } from 'next/router'
 import styles from '../../styles/posts.module.css'
+import { Toolbar } from '../../components/toolbar'
 
 export const Feed = ({pageNumber, articles}) => {
+
+    const router = useRouter();
 
     console.log(pageNumber)
     console.log(articles)
     return (
-        <div className={styles.main}>
-            {articles.map((article, index) => (
-                <div key={index} className={styles.post}>
-                    <h1>{article.title}</h1>
-                    <img src={article.urlToImage} alt={article.title}/>
+
+        <div className='page-container'>
+
+            <Toolbar/>
+
+            <div className={styles.main}>
+                {articles.map((article, index) => (
+                    <div key={index} className={styles.post}>
+                        <h1 onClick={() => (window.location.href = article.url)}>{article.title}</h1>
+                        <p>{article.description}</p>
+                        {!!article.urlToImage && <img src={article.urlToImage} alt={article.title}/>}
+                    </div>
+                ))}
+            </div>
+
+            <div className={styles.paginator}>
+                <div 
+                    onClick={() => {
+                        if(pageNumber > 1){
+                            router.push(`/feed/${pageNumber - 1}`)
+                        }
+                    }}
+                className={pageNumber === 1 ? styles.disabled : styles.active}
+                >
+                    Previous Page
                 </div>
-            ))}
+
+                <div>{pageNumber}</div>
+
+                <div 
+                    onClick={() => {
+                        if(pageNumber < 5){
+                            router.push(`/feed/${pageNumber + 1}`)
+                        }
+                    }}
+                className={pageNumber === 5 ? styles.disabled : styles.active}
+                >
+                    Next Page
+                </div>
+            </div>
         </div>
+        
     )
 }
 
